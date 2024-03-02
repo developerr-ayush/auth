135dd4855f0d0900e41ddcb2047dff71eba9d44d
shahayush480 <shahayush480@gmail.com>
2024-03-02 16:58:25 +0530

    fixed issue

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,7 +2,10 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  let blogs = await db.blog.findMany();
+  let blogs = await db.blog.findMany({
+    where: { status: "published" },
+    orderBy: { updatedAt: "desc" },
+  });
   if (!blogs) return NextResponse.json(new Error("Not Found"), { status: 404 });
   return NextResponse.json(blogs);
 }
--- components/blog/datatable.tsx
+++ components/blog/datatable.tsx
@@ -46,7 +46,7 @@ export const DataTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {!!data && Array.isArray(data) && data.map((row: DemoData) => {
+                    {!!data && data.map((row: DemoData) => {
                         return (
                             <TableRow key={row.id}>
                                 {/* <TableCell>{row.id}</TableCell> */}
@@ -54,6 +54,7 @@ export const DataTable = () => {
                                 <TableCell>{new Date(row.updatedAt).toLocaleDateString()}</TableCell>
                                 <TableCell>{row.status}</TableCell>
                                 <TableCell><Link href={`/admin/blog/${row.id}/edit`}><BsPen /></Link></TableCell>
+
                             </TableRow>
                         )
                     })}
