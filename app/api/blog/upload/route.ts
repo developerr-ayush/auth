2274a662c31d8bce7d2d4b87b9a60d8f5ce37544
shahayush480 <shahayush480@gmail.com>
2024-03-15 22:44:49 +0530

    change

--- app/api/blog/upload/route.ts
+++ app/api/blog/upload/route.ts
@@ -1,3 +1,4 @@
+import cloudinary from "@/lib/cloudinary";
 import { writeFile } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
@@ -21,8 +22,20 @@ export async function POST(req: NextRequest) {
 
   console.log(path, buffer, byte, file);
   const updatedPath = path.split("\\").join("/").split("public");
-  const updatedPathString = updatedPath[1];
+  let updatedPathString = updatedPath[1];
   console.log(updatedPathString);
+  await cloudinary.v2.uploader.upload(path, function (error, result) {
+    console.log(result, error);
+    if (!result) {
+      return NextResponse.json({
+        success: false,
+        message: "File not found",
+        code: 404,
+      });
+    }
+    updatedPathString = result.url;
+  });
+
   return NextResponse.json({
     success: true,
     time: Date.now(),
